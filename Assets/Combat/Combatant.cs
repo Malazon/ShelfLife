1dfc5d7dc7948c038d3b58bde39f4f0dce202b45
Craig Jones <wcjones@afterhourslab.com>
2019-10-12 14:28:17 -0500

    sync

--- Assets/Combat/Combatant.cs
+++ Assets/Combat/Combatant.cs
@@ -6,13 +6,20 @@ public class PlayerCombat : MonoBehaviour
     [SerializeField] private float maxHealth = 100;
     [SerializeField] private float recoveryRate = 50;
     [SerializeField] private float recoveryDelay = 5;
+    [SerializeField] private bool immortal = false;
     
     private float _health;
     private float _damageThisFrame = 0;
+    private float _damageLastFrame = 0;
     private float _lastDamageTime = 0;
+    private bool _hasDied = false;
 
-    public float DamageThisFrame => _damageThisFrame;
+    public bool HasDied => _hasDied;
 
+    // Delayed by 1 frame
+    public float DamageTaken => _damageLastFrame;
+
+    // Delayed by 1 frame
     public float Health => _health;
 
     public void Start()
@@ -22,12 +29,23 @@ public class PlayerCombat : MonoBehaviour
     
     public void LateUpdate()
     {
+        if (!immortal)
+        {
+            _health = Mathf.Clamp(_health - _damageThisFrame, 0, maxHealth);
+            
+            if (_health == 0)
+            {
+                _hasDied = true;
+            }
+        }
+        
+        _damageLastFrame = _damageThisFrame;
         _damageThisFrame = 0;
     }
 
     public void Update()
     {
-        if (Time.time - _lastDamageTime > recoveryDelay)
+        if (!immortal && !_hasDied && Time.time - _lastDamageTime > recoveryDelay)
         {
             _health = Mathf.Clamp(_health + recoveryDelay * Time.deltaTime, 0, maxHealth);
         }
@@ -36,6 +54,6 @@ public class PlayerCombat : MonoBehaviour
     public void DoDamage(float damage)
     {
         _damageThisFrame += damage;
-        lastDamageTime = Time.time;
+        _lastDamageTime = Time.time;
     }
 }
