2b4adb8f72302823f5f2810ca8da818ea0f7fc8e
Craig Jones <wcjones@afterhourslab.com>
2019-10-13 04:39:04 -0500

    zones now control lights

--- Assets/Camera & Zones/ZoneTrigger.cs
+++ Assets/Camera & Zones/ZoneTrigger.cs
@@ -1,14 +1,47 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
 
 public class ZoneTrigger : MonoBehaviour
 {
     [SerializeField] private Transform CameraLocation;
+    [SerializeField] private GameObject LightsParent;
 
+    private List<Light> lights;
+    private static List<Light> allLights;
+    
+    private void Start()
+    {
+        if (allLights == null)
+        {
+            allLights = new List<Light>();
+        }
+        
+        lights = new List<Light>();
+        
+        if (LightsParent == null) return;
+        
+        lights.AddRange(LightsParent.GetComponentsInChildren<Light>());
+        allLights.AddRange(lights);
+    }
+    
     private void OnTriggerEnter(Collider other)
     {
         if (CameraSingleton.Active == null) return;
 
         if (other.GetComponentInParent<PlayerSingleton>() != null)
             CameraSingleton.Active.TargetLocation = CameraLocation.transform;
+        
+        
+        
+        foreach (var l in allLights.Except(lights))
+        {
+            l.gameObject.SetActive(false);
+        }
+
+        foreach (var l in lights)
+        {
+            l.gameObject.SetActive(true);
+        }
     }
 }
