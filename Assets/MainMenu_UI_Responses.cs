80511bb2c2ef41a79fe29626284f67ca88af1b71
Todd Moulder <toddmoulder88@gmail.com>
2019-10-12 19:33:56 -0500

    Main Menu UI Callbacks created and set; build list start; Camera adjustments to fit with Global Scene positioning, Player height adjustments, player capsule refinement with new model,

---
+++ Assets/MainMenu_UI_Responses.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu_UI_Responses : MonoBehaviour
+{
+    public void OnPlayClicked()
+    {
+        SceneManager.LoadScene("Global");
+        SceneManager.LoadSceneAsync("PrettyTesting",LoadSceneMode.Additive);
+    }
+
+    public void OnAbout_Clicked()
+    {
+        // TODO:  Open About page!
+        Debug.LogWarning("OnAbout_Clicked() called but not implemented yet!");
+    }
+
+    public void OnExit_Clicked()
+    {
+        Application.Quit();
+    }
+}
