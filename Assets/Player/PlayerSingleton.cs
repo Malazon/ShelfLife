8fff862adca72153d67939e803a490d48aa783ae
Todd Moulder <toddmoulder88@gmail.com>
2019-10-12 23:15:51 -0500

    getting rid of getcomponent call

--- Assets/Player/PlayerSingleton.cs
+++ Assets/Player/PlayerSingleton.cs
@@ -24,7 +24,5 @@ public class PlayerSingleton : MonoBehaviour
             }
 
         Active = this;
-
-        _playerAnimatorCodeHook = GetComponentInChildren<PlayerAnimationCodeHook>();
     }
 }
