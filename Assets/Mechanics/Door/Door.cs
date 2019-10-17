d9d263d416bb4d9b40dd1688de49ea985efb1abe
Todd Moulder <toddmoulder88@gmail.com>
2019-10-16 21:26:30 -0500

    quick door fix.  ship it

--- Assets/Mechanics/Door/Door.cs
+++ Assets/Mechanics/Door/Door.cs
@@ -4,33 +4,153 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+    enum DoorState
+    {
+        Closed,
+        Ajar,
+        Open
+    };
+
     [SerializeField] private Transform Hinge;
 
     [SerializeField] private AudioClip[] openAndCloseClips = new AudioClip[2];
     [SerializeField] private AudioSource doorSoundSource = null;
 
     private float lastTriggerTime = 0;
+    private DoorState myState = DoorState.Closed;
+    
 
     public void Trigger()
     {
         lastTriggerTime = Time.time;
+
+        if(myState == DoorState.Closed)
+        {
+            myState = DoorState.Ajar;
+        }
     }
 
     void Update()
     {
-        if (Time.time - lastTriggerTime < 0.5f)
-        {
-            // Open?
-            HandleDoorSounds(openDoor: true);
+        UpdateDoorStateMachine();
+    }
 
-            Hinge.localRotation = Quaternion.RotateTowards(Hinge.localRotation, Quaternion.Euler(0, 90, 0), 720 * Time.deltaTime);
+    private void UpdateDoorStateMachine()
+    {
+        switch (myState)
+        {
+            case DoorState.Open:
+                {
+                    // too long?  back to ajar!
+                    if (Time.time - lastTriggerTime > 0.5f)
+                    {
+                        myState = DoorState.Ajar;
+                    }
+                }
+                break;
+            case DoorState.Ajar:
+                {
+                    if (Time.time - lastTriggerTime < 0.5f)
+                    {
+                        Quaternion targetQuat = Quaternion.Euler(0, 90, 0);
+                        // Open
+                        Hinge.localRotation = Quaternion.RotateTowards(Hinge.localRotation, targetQuat, 720 * Time.deltaTime);
+
+                        if (Mathf.Approximately(Quaternion.Angle(Hinge.localRotation, targetQuat), 0f))
+                        {
+                            ChangeState(DoorState.Open);
+                        }
+                    }
+                    else
+                    {
+                        // Close
+                        Hinge.localRotation = Quaternion.RotateTowards(Hinge.localRotation, Quaternion.identity, 720 * Time.deltaTime);
+
+                        if (Mathf.Approximately(Quaternion.Angle(Hinge.localRotation, Quaternion.identity), 0f))
+                        {
+                            ChangeState(DoorState.Closed);
+                        }
+                    }
+                }
+                break;
+            case DoorState.Closed:
+                {
+                    if (Time.time - lastTriggerTime < 0.5f)
+                    {
+                        ChangeState(DoorState.Ajar);
+                    }
+                }
+                break;
+            default: break;
         }
-        else
+        switch (myState)
         {
-            // Close?
-            HandleDoorSounds(openDoor: false);
+            case DoorState.Open:
+                {
+                    // too long?  back to ajar!
+                    if (Time.time - lastTriggerTime > 0.5f)
+                    {
+                        myState = DoorState.Ajar;
+                    }
+                }
+                break;
+            case DoorState.Ajar:
+                {
+                    if (Time.time - lastTriggerTime < 0.5f)
+                    {
+                        Quaternion targetQuat = Quaternion.Euler(0, 90, 0);
+                        // Open
+                        Hinge.localRotation = Quaternion.RotateTowards(Hinge.localRotation, targetQuat, 720 * Time.deltaTime);
 
-            Hinge.localRotation = Quaternion.RotateTowards(Hinge.localRotation, Quaternion.identity, 720 * Time.deltaTime);
+                        if (Mathf.Approximately(Quaternion.Angle(Hinge.localRotation, targetQuat), 0f))
+                        {
+                            ChangeState(DoorState.Open);
+                        }
+                    }
+                    else
+                    {
+                        // Close
+                        Hinge.localRotation = Quaternion.RotateTowards(Hinge.localRotation, Quaternion.identity, 720 * Time.deltaTime);
+
+                        if (Mathf.Approximately(Quaternion.Angle(Hinge.localRotation, Quaternion.identity), 0f))
+                        {
+                            ChangeState(DoorState.Closed);
+                        }
+                    }
+                }
+                break;
+            case DoorState.Closed:
+                {
+                    if (Time.time - lastTriggerTime < 0.5f)
+                    {
+                        ChangeState(DoorState.Ajar);
+                    }
+                }
+                break;
+            default: break;
+        }
+
+    }
+
+    private void ChangeState(DoorState newState, bool silent = false)
+    {
+        myState = newState;
+
+        if (!silent)
+        {
+            switch (newState)
+            {
+                case DoorState.Open:
+                    {
+                        HandleDoorSounds(openDoor: true);
+                    }
+                    break;
+                case DoorState.Closed:
+                    {
+                        HandleDoorSounds(openDoor: false);
+                    }
+                    break;
+            }
         }
     }
 
